7b7691097e20b8595edfdb07e26a1b0abcb44300
rndev80 <rndev80@gmail.com>
2019-05-01 11:57:12 +0300

    Implemented graphql query

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 
 const LAUNCHES_QUERY = gql`
     query LaunchesQuery {
@@ -16,7 +17,7 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
             {
@@ -25,11 +26,17 @@ export class Launches extends Component {
                     if (error) console.log(error);
 
                     console.log(data);
-                    return <h1>Test</h1>;
+                    return (
+                        <Fragment>
+                        {
+                            data.launches.map(launch => <LaunchItem key={launch.flight_number} launch={launch} />)
+                        }
+                        </Fragment>
+                    );
                 }
             }
         </Query>
-      </div>
+      </Fragment>
     )
   }
 }
