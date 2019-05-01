0409ac075b2d664c72991b2d395e9e102208800f
rndev80 <rndev80@gmail.com>
2019-05-01 13:09:22 +0300

    Implemented detailed launch with graphql

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 import './App.css';
 import logo from './logo.jpg';
 
@@ -21,6 +22,7 @@ function App() {
             style={{ width: 300, display: 'block', margin: 'auto' }}
           />
           <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
         </div>
       </Router>
     </ApolloProvider>
