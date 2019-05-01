c2e5fe320d10b74c6befc5f4852c0e5aa44268c5
rndev80 <rndev80@gmail.com>
2019-05-01 11:19:04 +0300

    Added logo and title

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.jpg';
 
 function App() {
   return (
     <div className="App">
-      <h1>SpaceX</h1>
+      <img
+        src={logo}
+        alt="SpaceX"
+        style={{ width: 300, display: 'block', margin: 'auto' }}
+      />
     </div>
   );
 }
