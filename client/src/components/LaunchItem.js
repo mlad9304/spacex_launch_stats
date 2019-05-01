bdf6da916a6f111de794c347b74d2d00838ac57b
rndev80 <rndev80@gmail.com>
2019-05-01 12:15:13 +0300

    Updated launch date format

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import Moment from 'react-moment';
 
 export default function LaunchItem({ launch: { flight_number, mission_name, launch_date_local, launch_success } }) {
     return (
@@ -10,7 +11,7 @@ export default function LaunchItem({ launch: { flight_number, mission_name, laun
                         'text-success': launch_success,
                         'text-danger': !launch_success
                     })}>{ mission_name }</span></h4>
-                    <p>Date: { launch_date_local }</p>
+                    <p>Date: <Moment format="YYYY-MM-DD HH:mm">{ launch_date_local }</Moment></p>
                 </div>
                 <div className="col-md-3">
                     <button className="btn btn-secondary">Launch Details</button>
