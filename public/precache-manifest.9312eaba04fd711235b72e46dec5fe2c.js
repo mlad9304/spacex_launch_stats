33071ab79bc5a771ed94fd5ec5fd2147b28c1a5d
rndev80 <rndev80@gmail.com>
2019-05-01 13:18:09 +0300

    Impmented deployment

---
+++ public/precache-manifest.9312eaba04fd711235b72e46dec5fe2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac76c0c11c0da580ad1257d0143751a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b7fb9dd90bbe373a2ba",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "be6ce0260394620c6ce7",
+    "url": "/static/js/2.f18cc709.chunk.js"
+  },
+  {
+    "revision": "8b7fb9dd90bbe373a2ba",
+    "url": "/static/js/main.97a635e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b15ef94746031f8853af907909881f2a",
+    "url": "/static/media/logo.b15ef947.jpg"
+  }
+]);
